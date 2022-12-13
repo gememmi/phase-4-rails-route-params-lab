1f4d3d177b215d3e7d283d9e8f023c52ecb9243b
Emily <emelizadaniels@gmail.com>
2022-12-13 11:28:30 -0500

    commit tree 4805466bf5fed9feda4c1cf5db70b23715f7af83

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -5,4 +5,14 @@ class StudentsController < ApplicationController
     render json: students
   end
 
+  def show
+    student = Student.find(params[:id])
+    render json: student
+  end
+
+  def name
+    student = Student.where(first_name: params[:name]) || Student.where(last_name: params[:name])
+    render json: student
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,18 @@ Rails.application.routes.draw do
   get '/students', to: 'students#index'
   get '/students/grades', to: 'students#grades'
   get '/students/highest-grade', to: 'students#highest_grade'
+  get '/students/:id', to: 'students#show'
+  get '/students?name=:first_name', to: 'students#name'
+  get '/students?name=:name', to: 'students#name'
 end
+
+
+# context 'with query params' do
+#   it 'returns students whose first or last name matches the query' do
+#     get '/students?name=kirby'
+
+#     expect(response.body).to include_json([
+#       { first_name: 'Vanessa', last_name: 'Kirby', grade: 85 }
+#     ])
+#   end
+# end
